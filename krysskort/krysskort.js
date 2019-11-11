b0f134a2fd67007340efe56ced08a77b55433764
Gavia1981 <j_sodercrantz@hotmail.com>
2019-11-12 00:14:21 +0100

    Update krysskort.js

--- krysskort/krysskort.js
+++ krysskort/krysskort.js
@@ -1,6 +1,6 @@
 var krysskort = {
     loadSpeciesList: function() {
-        const response = await fetch('specieslist.json');
+        const response = await fetch('https://cdn.jsdelivr.net/gh/Gavia1981/gavia1981.github.io/krysskort/specieslist.json');
         const myJson = await response.json();
         console.log(myJson);
         console.log("now!");
